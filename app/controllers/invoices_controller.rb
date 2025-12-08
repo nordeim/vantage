430e815a4be38a403f9b15f240ba1c76f31f943a
heinazhik <heinazhikweymong@outlook.com>
2025-12-08 20:38:38 +0800

    invoices_controller.rb

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,16 +1,36 @@
 # app/controllers/invoices_controller.rb
 class InvoicesController < ApplicationController
   def index
-    render inertia: 'Invoices/Index'
+    # For Day 4, we're using mock data on frontend
+    # But prepare the structure for when we have real data
+    render inertia: 'Invoices/Index', props: {
+      # These will be populated with real data later
+      # invoices: serialize_invoices(Invoice.all)
+    }
   end
 
   def new
-    render inertia: 'Invoices/New'
+    # Day 5 expects clients for the selector
+    render inertia: 'Invoices/New', props: {
+      clients: [], # Will be Client.all.map { |c| serialize_client(c) }
+      invoiceNumber: generate_invoice_number
+    }
   end
 
   def edit
+    # Day 5 expects full invoice data
     render inertia: 'Invoices/Edit', props: {
-      id: params[:id]
+      id: params[:id],
+      # invoice: serialize_invoice(Invoice.find(params[:id])),
+      # clients: Client.all.map { |c| serialize_client(c) }
     }
+  end
+
+  private
+
+  def generate_invoice_number
+    year = Date.today.year
+    # Simple sequence for now
+    "INV-#{year}-#{rand(1000..9999)}"
   end
 end
